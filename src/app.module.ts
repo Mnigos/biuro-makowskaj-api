0c580bf50808e1d72d15e1d4fd1d619825ee9eed
MoneyIgos <makowskiigor@gmail.com>
2020-11-30 19:35:19 +0100

    perf: delete unused module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [PostsController],
+  providers: [PostsService],
 })
 export class AppModule {}
