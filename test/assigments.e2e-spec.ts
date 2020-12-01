697661336bd42ea9f35c574d458020725c257e13
MoneyIgos <makowskiigor@gmail.com>
2020-12-01 14:16:53 +0100

    test: add e2e tests for /posts endpoint

---
+++ test/assigments.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { CreateAssigmentDto } from '../src/dto/create-assigment.dto';
+
+describe('Getting assigments (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET assigments', () => {
+    return request(app.getHttpServer()).get('/posts').expect(200);
+  });
+
+  it('/POST assigments when everything is ok', () => {
+    const assigment: CreateAssigmentDto = {
+      title: 'title',
+      description: 'description',
+      image: 'image',
+    };
+
+    return request(app.getHttpServer())
+      .post('/posts')
+      .set('Content-Type', 'application/json')
+      .send(assigment)
+      .expect(201)
+      .expect(assigment);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
