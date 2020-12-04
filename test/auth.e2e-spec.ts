0e4f2dc4607ea8062be73d91d2f6301d091a015d
MoneyIgos <makowskiigor@gmail.com>
2020-12-04 13:58:56 +0100

    test: add e2e tests for auth

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,75 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { getModelToken } from '@nestjs/mongoose';
+import User from '../src/models/user.model';
+import { CreateUserDto } from '../src/dto/create-user.dto';
+import { resolve } from 'path';
+
+describe('Auth (e2e)', () => {
+  let app: INestApplication;
+
+  const user: CreateUserDto = {
+    username: 'Max',
+    password: 'sFly',
+  };
+
+  const secondUser: CreateUserDto = {
+    username: 'SecondMax',
+    password: 'Fly',
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [
+        {
+          provide: getModelToken('Assigment'),
+          useValue: User,
+        },
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Trying to get user and getting Unauthorized', () => {
+    return request(app.getHttpServer()).get('/user').expect(401);
+  });
+
+  it('Trying to log in and getting Unauthorized', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(secondUser)
+      .expect(401)
+      .expect(({ body }) => {
+        expect(body.access_token).toBeUndefined();
+      });
+  });
+
+  it('Registering a user', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send(user)
+      .expect(201)
+      .expect(({ body }) => {
+        expect(body).toBeDefined();
+      });
+  });
+
+  it('Logging a user', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(user)
+      .expect(201)
+      .expect(({ body }) => {
+        expect(body.access_token).toBeDefined();
+      });
+  });
+});
